62aaec67e893dea749683fe1ecc2dc8f1e73f69c
Ming-Nu <mingnul@sfu.ca>
2016-11-22 23:06:39 -0800

    Changed hosting sessions page to my sessions page, welcome to test

--- public/hostingSessions.js
+++ public/hostingSessions.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
 	apiKey: "AIzaSyCCcJUaBLram1g9zoqTUVkK9K-iHyv4V-A",
 	authDomain: "studywitme-f268e.firebaseapp.com",
@@ -8,64 +7,65 @@ var config = {
 };
 firebase.initializeApp(config);
 
-function initApp() {
-	console.log("view sessions page")
-
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			console.log("user: " + user.uid + " isAnonymous: " + user.isAnonymous)
-			setupView()
-		} else {
-			console.log("signed out")
-		}
-	})
-
-	document.getElementById('back_to_profile_btn').addEventListener('click', gotoProfile, false);
-}
-
 function setupView() {
 
-	let user = firebase.auth().currentUser;
-
+	var user = firebase.auth().currentUser;
+	
 	if (user != null) {
 		var hostingSessionsRef = firebase.database().ref().child('users').child(user.uid).child('hosting_sessions');
+		var joinedSessionsRef = firebase.database().ref().child('users').child(user.uid).child('joined_sessions');
 
 		hostingSessionsRef.on('child_added', function(data) {
-			// console.log(data.key)
-			let sessionRef = firebase.database().ref('sessions').child(data.key).child('metadata');
-			sessionRef.once('value', function(snapshot) {
+			//console.log(data.key)
+			var hostSessionRef = firebase.database().ref('sessions').child(data.key).child('metadata');
+			hostSessionRef.once('value', function(snapshot) {
 				// console.log(snapshot.val())
-				let dict = snapshot.val()
+				var dict = snapshot.val()
 				// console.log(dict)
-				let tableBody = document.getElementById("tableBody")
-				addRow(tableBody, dict['course'], dict['campus'], "x")
+				var table = document.getElementById("hostedTable")
+				var row = document.getElementById("hostedRow")
+				addBox(row, dict['course'], dict['campus'], dict['host_id'])
 			})
 		});
+
+		joinedSessionsRef.on('child_added', function(data)
+		{
+			var joinSessionRef = firebase.database().ref('sessions').child(data.key).child('metadata');
+			joinSessionRef.once('value', function(snapshot)
+			{
+				var dict = snapshot.val()
+				var table = document.getElementById("joinedTable")
+				var row = document.getElementById("joinedRow")
+				addBox(row, dict['course'], dict['campus'], dict['host_id'])
+			})
+		})
 	}
 }
 
-function addRow(tableBody, col1val, col2val, col3val)
+
+function addBox(row, course, campus, desc)
 {
-	let row = document.createElement("tr");
-	let cell1 = document.createElement("td");
-	let cell2 = document.createElement("td");
-	let cell3 = document.createElement("td");
-	let textnode1 = document.createTextNode(col1val);
-	let textnode2 = document.createTextNode(col2val);
-	let textnode3 = document.createTextNode(col3val);
-	cell1.appendChild(textnode1);
-	cell2.appendChild(textnode2);
-	cell3.appendChild(textnode3);
-	row.appendChild(cell1);
-	row.appendChild(cell2);
-	row.appendChild(cell3);
-	tableBody.appendChild(row);
+	var divNew = document.createElement("div");
+	divNew.className = "box";
+	var buttonNew = document.createElement("input");
+	buttonNew.type = "button";
+	buttonNew.value = "View Session";
+	buttonNew.className = "button";
+	var box = document.createElement("a");
+	var textNode = document.createElement("center");
+	textNode.className = "text";
+	textNode.innerHTML = "Course: " + course + "<br>" + "Campus: " + campus + "<br>" + "Host: " + desc;
+	divNew.appendChild(box);
+	divNew.appendChild(buttonNew);
+	divNew.appendChild(textNode);
+	row.appendChild(divNew);
 }
 
+
 function gotoProfile() {
-	window.location = "main.html"
+	window.location = "main.html" 
 }
 
 window.addEventListener('load', function() {
-    initApp()
-});
+    setupView();
+}); 
