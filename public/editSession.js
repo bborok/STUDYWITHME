0ae94a4c5c367c7cf0366613edeb6274bd97b626
Bobae Choi <bobaec@sfu.ca>
2016-11-24 00:58:15 -0800

    all links finally work, just need css/messenger

--- public/editSession.js
+++ public/editSession.js
@@ -6,7 +6,6 @@ var config = {
 	messagingSenderId: "1009156773779"
 };
 firebase.initializeApp(config);
-
 var sessionID;
 
 function initApp() {
@@ -18,19 +17,17 @@ function initApp() {
 		    setupViews();
 		} else {
 		    console.log("signed out");
-        window.location = 'login.html'
-
 		}
 	});
 
-    // document.getElementById('create_session_btn').addEventListener('click', editSession, false);
+    //document.getElementById('create_session_btn').addEventListener('click', editSession, false);
     document.getElementById('view_profile_btn').addEventListener('click', gotoProfilePage, false);
     document.getElementById('signout_btn').addEventListener('click', signOutUser, false);
-  	document.getElementById('discover_sessions_btn').addEventListener('click', gotoDiscoverSessionsPage, false);
-  	document.getElementById('create_session_btn').addEventListener('click', gotoCreateSessionPage, false);
-  	document.getElementById('pending_sessions_btn').addEventListener('click', gotoPendingSessionsPage, false);
-  	document.getElementById('joined_sessions_btn').addEventListener('click', gotoJoinedSessionsPage, false);
-  	document.getElementById('hosting_sessions_btn').addEventListener('click', gotoHostingSessionsPage, false);
+    document.getElementById('discover_sessions_btn').addEventListener('click', gotoDiscoverSessionsPage, false);
+    document.getElementById('create_session_btn').addEventListener('click', gotoCreateSessionPage, false);
+    document.getElementById('pending_sessions_btn').addEventListener('click', gotoPendingSessionsPage, false);
+    document.getElementById('joined_sessions_btn').addEventListener('click', gotoJoinedSessionsPage, false);
+    document.getElementById('hosting_sessions_btn').addEventListener('click', gotoHostingSessionsPage, false);
 }
 
 //populates the form with the session's values
@@ -62,33 +59,33 @@ function setupViews() {
 	}
     var data = firebase.database().ref("sessions/"+sessionID+"/metadata/");
     data.once("value").then(function(snapshot){
-  	document.getElementById("inputCourse").value = snapshot.child("course").val().substring(0,4).toUpperCase();
-  	document.getElementById("inputCourseNum").value = snapshot.child("course").val().substring(5);
+	document.getElementById("inputCourse").value = snapshot.child("course").val().substring(0,4).toUpperCase();
+	document.getElementById("inputCourseNum").value = snapshot.child("course").val().substring(5);
 
-  	let startTimeStamp = snapshot.child("start_date").val();
-  	let startDate = new Date(Number(startTimeStamp));
-  	let startDateUsable = startDate.toJSON().substring(0,10);
-  	startDateUsable = startDateUsable.split('-');
-  	let startTime = new Date(Number(startTimeStamp));
-  	let startTimeUsable = startDate.toLocaleTimeString().split(' ');
-  	document.getElementById("inputStartDate").value = startDateUsable[1]+'/'+startDateUsable[2]+'/'+startDateUsable[0];
-  	document.getElementById("inputStartTime").value = startTimeUsable[0];
+	let startTimeStamp = snapshot.child("start_date").val();
+	let startDate = new Date(Number(startTimeStamp));
+	let startDateUsable = startDate.toJSON().substring(0,10);
+	startDateUsable = startDateUsable.split('-');
+	let startTime = new Date(Number(startTimeStamp));
+	let startTimeUsable = startDate.toLocaleTimeString().split(' ');
+	document.getElementById("inputStartDate").value = startDateUsable[1]+'/'+startDateUsable[2]+'/'+startDateUsable[0];
+	document.getElementById("inputStartTime").value = startTimeUsable[0];
 
-  	document.getElementById("inputDuration").value = (snapshot.child("end_date").val() - startTimeStamp) / 1000 / 3600;
-  	document.getElementById("inputDesc").value = snapshot.child("description").val();
+	document.getElementById("inputDuration").value = (snapshot.child("end_date").val() - startTimeStamp) / 1000 / 3600;
+	document.getElementById("inputDesc").value = snapshot.child("description").val();
 
-  	let select = document.getElementById("inputLoc");
-  	let locOld = snapshot.child("campus").val();
-  	if (locOld == "Surrey"){
-  	    select.selectedIndex = 0;
-  	} else if (locOld == "Burnaby"){
-  	    select.selectedIndex = 1;
-  	} else {
-  	    select.selectedIndex = 2;
-  	}
+	let select = document.getElementById("inputLoc");
+	let locOld = snapshot.child("campus").val();
+	if (locOld == "Surrey"){
+	    select.selectedIndex = 0;
+	} else if (locOld == "Burnaby"){
+	    select.selectedIndex = 1;
+	} else {
+	    select.selectedIndex = 2;
+	}
 
-  	document.getElementById("inputExactLocation").value = snapshot.child("exact_location").val();
-  	document.getElementById("inputMaxAttend").value = snapshot.child("max_people").val();
+	document.getElementById("inputExactLocation").value = snapshot.child("exact_location").val();
+	document.getElementById("inputMaxAttend").value = snapshot.child("max_people").val();
     });
 }
 
@@ -98,22 +95,22 @@ function editSession() {
 
     //should be handled more elegantly
     if (course.length < 4){
-	     console.log("invalid course code");
+	console.log("invalid course code");
     }
-    course += " " + document.getElementById('inputCourseNum');
+    course += " " + document.getElementById('inputCourseNum').value;
 
-    let campus = document.getElementById('inputCampus').value;
+    let campus = document.getElementById('inputLoc').value;
     let startDate = document.getElementById('inputStartDate').value;
     let startTime = document.getElementById('inputStartTime').value;
     let fullStartDate = function() {
-    	let dateStr = startDate + " " + startTime;
-    	let date = new Date(dateStr);
-    	return date;
-        }();
+	let dateStr = startDate + " " + startTime;
+	let date = new Date(dateStr);
+	return date;
+    }();
     let startTimestamp = fullStartDate.getTime();
     let duration = document.getElementById('inputDuration').value;
     let endTimestamp = startTimestamp + (duration * 3600 * 1000);
-    let description = document.getElementById('inputDescription').value;
+    let description = document.getElementById('inputDesc').value;
     let exactLocation = document.getElementById('inputExactLocation').value;
     let maxGuests = document.getElementById('inputMaxAttend').value;
 
@@ -139,24 +136,7 @@ function editSession() {
 
     firebase.database().ref("/sessions/"+sessionID+"/metadata/").set(session);
 
-    // console.log(sessionObj)
-
-    /*let newSessionKey = firebase.database().ref().child('sessions').push().key;
-
-    var fanoutObject = {}
-    fanoutObject['/users/'+user.uid+'/hosting_sessions/'+newSessionKey] = true;
-    fanoutObject['/sessions/'+newSessionKey+'/metadata'] = sessionObj;
-
-    // console.log(fanoutObject)
-
-    let updatePromise = firebase.database().ref().update(fanoutObject);
-
-    updatePromise.then(function() {
-	console.log("session created!");
-	window.location = 'main.html';
-    }, function(error) {
-	console.log(error);
-    })*/
+    window.location = "joinedSessions.html";
 }
 
 function gotoProfilePage() {
