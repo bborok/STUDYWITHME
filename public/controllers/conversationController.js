9670e3dd50724bfcc2b00647906ee016815f7c85
Kostiantyn Koval <kkoval@sfu.ca>
2016-11-29 14:47:47 -0800

    Integrate chat functionality into the Conversation.html

--- public/controllers/conversationController.js
+++ public/controllers/conversationController.js
@@ -1,10 +1,23 @@
+const USER_ID = "Somebody";
+
 angular.module("StudyWitMe", ['firebase', 'ngRoute'])
     .constant("FB_URL", 'https://cmpt106.firebaseio.com/')
     .factory('shareConversation', function () {
         return {id: 'DEFAULT'};
     })
+    // .config(['$routeProvider', function ($routeProvider) {
+    //     $routeProvider.when('/conversation', {
+    //         templateUrl: '/public/assets/templates/conversations.html'
+    //     }).when('/chat', {
+    //         templateUrl: '/public/assets/templates/chat.html'
+    //     })
+    //     .otherwise({
+    //          redirectTo: '/conversations'
+    //     });
+    // }
+    // ])
     .controller("ConversationController", function ($scope, $firebase, shareConversation, FB_URL) {
-        alert("started");
+        $scope.displayChat = false;
         $scope.dbSession = $firebase(new Firebase(FB_URL + "sessions"));
         $scope.dbConvers = $firebase(new Firebase(FB_URL + "conversations"));
 
@@ -41,8 +54,47 @@ angular.module("StudyWitMe", ['firebase', 'ngRoute'])
         $scope.openChat = function (id) {
             //Send conversation id of the picked session
             shareConversation.id = getSessionById(FB_URL, id).conversationId;
+            $scope.displayChat = true;
+
+            // })
+            // .controller("MessageController", function ($scope, $firebase, shareConversation, FB_URL) {
+
+            //Get conversation id from user selection
+            var conversationId = shareConversation.id;
+
+            $scope.dbMessages = $firebase(new Firebase(FB_URL + "conversations/" + conversationId + "/messages"));
+            $scope.messagesExist = false;
+
+            $scope.dbMessages.$on('value', function () {
+                $scope.messages = getMessagesByConversId(FB_URL, conversationId);
+                $scope.messagesExist = $scope.messages.length != 0;
+            });
+
+            $scope.sendMessage = function (event) {
+                if (event.which == 13 || event.keyCode == 13) {
+                    var textEntered = $scope.message.trim();
+                    if (textEntered.length > 0) {
+                        $scope.dbMessages.$add({
+                            owner: USER_ID,
+                            text: textEntered,
+                            date: new Date()
+                        });
+                        $scope.message = "";
+                        $scope.messageBox.value = '';
+                    }
+                }
+            }
+
         };
     });
+
+function getMessagesByConversId(FB_URL, id) {
+    var result;
+    new Firebase(FB_URL + 'conversations/' + id).once('value', function (snap) {
+        result = snap.val();
+    });
+    return result.messages;
+}
 
 function getSessionById(FB_URL, id) {
     var result;
@@ -51,3 +103,6 @@ function getSessionById(FB_URL, id) {
     });
     return result;
 }
+
+
+
